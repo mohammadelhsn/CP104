e219036d61da7dddf855763052b4022908d120d9
Mohammad El-Hassan <mohammadelhsn@gmail.com>
2023-09-15 16:28:09 -0400

    assignment

---
+++ elha7950_a01/src/t01.py
@@ -0,0 +1,25 @@
+"""
+-------------------------------------------------------
+Assignment 1, Task 1
+
+Description: 
+    This program demonstrates your understanding of the use of single, double and
+    triple quotes by using four (4) print functions to print the following four 
+    pieces of text:
+-------------------------------------------------------
+Author:  Mohammad El-Hassan
+ID:      169067950
+Email:   elha7950@mylaurier.ca
+__updated__ = "2023-09-15"
+-------------------------------------------------------
+"""
+
+print('What is a "program"?')
+print("""A program is a sequence of instructions that specifies how to perform a computation. The details look different in different languages, but a few basic instructions appear in just about every language:
+input: Get data from the keyboard, a file, or some other device.
+output: Display data on the screen or send data to a file or other device.
+math: Perform basic mathematical operations like addition and multiplication.
+conditional execution: Check for certain conditions and execute the appropriate code.
+repetition: Perform some action repeatedly, usually with some variation.""")
+print("Believe it or not, that's pretty much all there is to it.")
+print('''"You have enemies? Good. That means you've stood up for something, sometime in your life." Winston Churchill''')
---
+++ elha7950_a01/src/t02.py
@@ -0,0 +1,23 @@
+"""
+-------------------------------------------------------
+Assignment 1, Task 2
+
+Description: 
+    This program asks the user for their age and their favourite band, in that 
+    order, and then prints their answers
+-------------------------------------------------------
+Author:  Mohammad El-Hassan
+ID:      169067950
+Email:   elha7950@mylaurier.ca
+__updated__ = "2023-09-15"
+-------------------------------------------------------
+"""
+
+# Get the user input
+
+age = int(input("What is your age? "))
+fav_band = input("What is your favourite band? ")
+
+# Output the results
+
+print(f"I am {age} years old and {fav_band} is my favourite band.")
